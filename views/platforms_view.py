e36449bd385a64aea7f55bfcd9f86339cad79803
Mark Bromley <mark_bromley@ymail.com>
2016-09-17 23:46:54 +0100

    Updated platforms view.

--- views/platforms_view.py
+++ views/platforms_view.py
@@ -1,22 +1,20 @@
-# Import all the relevant libraries
 import pygame
 from easypg.sprites import Sprite
 
 class PlatformDesign():
-    """Class to build the entire level's platform as specified by the details
+    """Build the level's platform as specified by the details
     given in the platform model. This class is responsible for generating 
     individual sprites passed and assigning specific position based on the 
     data return by the model for the current level."""
 
     def __init__(self, model):
-        """Method to initialise the object, bind the model to the instance 
-        and get the corresponding values for each sprite from the model. 
+        """Gets corresponding values for each sprite from the model. 
         Additionally initialises the class properties."""
         # Add the model to the object
         self.model = model
         # Get the level platform design from the model for the current level
         self.levelPlatform = model.get_level_platform()
-        # Get each charactor's corresponding value
+        # Get each character's corresponding value
         self.block = self.model.get_char_representations("block")
         self.worm = self.model.get_char_representations("worm")
         self.coin = self.model.get_char_representations("coin")
@@ -31,31 +29,31 @@ class PlatformDesign():
         self.heightScaleFactor =1
 
     def get_level_block_positions(self):
-        """Method to return the positions of the block sprites."""
+        """Returns positions of block sprites."""
         return self._get_level_sprite_positions(self.block)
 
     def get_level_worm_positions(self):
-        """Method to return the positions of the worm sprites."""
+        """Returns positions of worm sprites."""
         return self._get_level_sprite_positions(self.worm)
 
     def get_level_coin_positions(self):
-        """Method to return the positions of the coin sprites."""
+        """Returns positions of coin sprites."""
         return self._get_level_sprite_positions(self.coin)
 
     def get_level_invis_positions(self):
-        """Method to return the positions of the invisible block sprites."""
+        """Return positions of invisible block sprites."""
         return self._get_level_sprite_positions(self.invisBlock)
 
     def get_level_castle_positions(self):
-        """Method to return the positions of the castle sprites."""
+        """Returns positions of castle sprites."""
         return self._get_level_sprite_positions(self.castle)
 
     def get_level_mouse_positions(self):
-        """Method to return the positions of the mouse sprites."""
+        """Return positions of mouse sprites."""
         return self._get_level_sprite_positions(self.mouse)
 
     def get_level_end_point(self):
-        """Method to return the end point of the level i.e. the point at 
+        """Returns the end point of the level i.e. the point at 
         which the player has finished the level and won."""
         if self.endPoint == None:
             self._get_level_sprite_positions(self.block)
@@ -65,14 +63,14 @@ class PlatformDesign():
             return self.endPoint
 
     def _get_max_abstract_size(self):
-        """Private method to get the maximum level platform width and height 
+        """Get the maximum level platform width and height 
         at any point from the data supplied by the model. Note these are 
         relative units and do not yet correspond to pixel value positions."""
         self.abstractLevelMaxHeight = len(self.levelPlatform)
         self.abstractLevelMaxWidth = max((len(line) for line in self.levelPlatform))
 
     def _get_scale_factors(self):
-        """Private method to get the scale factors for calculating the pixel 
+        """Get the scale factors for calculating the pixel 
         positions for each sprite within the level. This invokes another 
         internal method to retrieve the maximum relative width and then 
         calculates a scale factor for the game window size."""
@@ -84,7 +82,7 @@ class PlatformDesign():
         self.heightScaleFactor = max_height/ self.abstractLevelMaxHeight
 
     def _get_level_sprite_positions(self, element):
-        """Private method to return the position of every instance of a 
+        """Returns the position of every instance of a 
         given sprite in the current level according to the game model. A 
         sprite value can be passed in to check for its corresponding list 
         of positions. Returns a list of 2 pair tuples."""
